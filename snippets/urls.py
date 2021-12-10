24f326395703f9d46f8a07312ff1ea8038de5bf6
thisisazeez <imfostersyndrome@gmail.com>
2021-12-10 07:30:21 +0100

    added urls

---
+++ snippets/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+]
