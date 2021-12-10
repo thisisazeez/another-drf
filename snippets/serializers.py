33ce852eb7c8957e9b17527e624c7e5c4cd21663
thisisazeez <imfostersyndrome@gmail.com>
2021-12-10 07:22:42 +0100

    Done with models and views and serializers

---
+++ snippets/serializers.py
@@ -0,0 +1,10 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'laguage', 'style')
+        
