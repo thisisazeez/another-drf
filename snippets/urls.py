9984b35735e98f0ec0ce55dd3b30090eb5fe3d8c
thisisazeez <imfostersyndrome@gmail.com>
2021-12-10 07:31:08 +0100

    updates for urls

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,3 +6,5 @@ urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
